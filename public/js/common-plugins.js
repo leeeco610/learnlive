4035e15219e61ee8b68bc23fb1d09f58c7902f2c
Leeeco <759539395@qq.com>
2016-07-08 10:16:21 +0800

    demo label update

--- public/js/common-plugins.js
+++ public/js/common-plugins.js
@@ -1,25 +1,9 @@
 ﻿/**
  * 主要用于移动端
  * 引入前需引入Jquery  或 Zepto
- * Created by zykj_09 on 2015/9/25.
+ * Created by Leeeco on 2015/9/25.
  */
 var domain = "";
-/**
- *设置元素动画时间
- */
-$.fn.setAnimationTime = function(time){
-    if(this.length == 0) return ;
-    return $.each(this,function(i,e){
-        if(time == 0 || time == "none"){
-            e.style.webkitTransition = "none";
-            e.style.transition = "none";
-        }else{
-            e.style.webkitTransition = "all "+time+"s ease";
-            e.style.transition = "all "+time+"s ease";
-        }
-    });
-};
-
 //=================================扩展方法=================================
 Date.prototype.format = function (format) {
     var o = {
@@ -42,6 +26,21 @@ Date.prototype.format = function (format) {
     return format;
 };
 //==============================jQ公用插件==========================================
+/**********************************************************
+ *     设置元素动画时间
+ *********************************************************/
+$.fn.setAnimationTime = function(time){
+    if(this.length == 0) return ;
+    return $.each(this,function(i,e){
+        if(time == 0 || time == "none" || time == null){
+            e.style.webkitTransition = "none";
+            e.style.transition = "none";
+        }else{
+            e.style.webkitTransition = "all "+time+"s ease";
+            e.style.transition = "all "+time+"s ease";
+        }
+    });
+};
 /**********************************************************
  * 当前设备浏览器监测
  * @type {{versions: {qq, uc, weibo, weixin, trident, presto, webKit, gecko, mobile, ios, android, iPhone, iPad, webApp}, isAndroid: $.brower.isAndroid, isIOS: $.brower.isIOS, isPC: $.brower.isPC, isWeixin: $.brower.isWeixin, isQQ: $.brower.isQQ, isWeibo: $.brower.isWeibo}}
@@ -106,13 +105,13 @@ $.getUrlParam = function(name){
 /***************************************
  * 输入框元素获得焦点
  ***************************************/
-$.domFocus = function (targetElement) {
+$.fn.domFocus = function () {
     var length;
-    if ($.isIOS() && targetElement.setSelectionRange) {
-        length = targetElement.value.length;
-        targetElement.setSelectionRange(length, length);
+    if ($.isIOS() && this.setSelectionRange) {
+        length = this.value.length;
+        this.setSelectionRange(length, length);
     } else {
-        targetElement.focus();
+        this.focus();
     }
 };
 /******************************************
@@ -122,12 +121,13 @@ $.wechatSharePage = function () {
     var winH = $(window).height(),
         html='';
     html+='<div id="sharewarp" style="position: fixed;top:0;left:0;z-index: 99999;height:100px;width: 100%;height:'+winH+'px;background: rgba(0,0,0,0.6);">';
-    html+=   '<p style="padding: 15px 20px 20px; background:#fff;text-align:right;margin: 0;"><span style="display:inline-block;font-family:Microsoft Yahei;padding-right: 20px;font-size: 17px;color: #212121">点击右上角，分享到朋友圈</span><img src="img/iconfont-jiantou.png" style="height: 60px;"> </p>';
+    html+=   '<p style="padding: 15px 20px 20px; background:#fff;text-align:right;margin: 0;"><span style="display:inline-block;font-family:Microsoft Yahei;padding-right: 20px;font-size: 17px;color: #212121">点击右上角，分享到朋友圈</span><img src="/public/img/iconfont-jiantou.png" style="height: 60px;"> </p>';
     html+='</div>';
     $("body").css("overflow","hidden").append($(html));
     var $allPage = $("div[data-role='page']");
     $allPage.css({"overflow-y":"hidden","max-height":"100%"});
-    new FastButton(document.querySelector("#sharewarp"),function(){
+
+    $("#sharewarp").click(function () {
         $("body").css("overflow","auto");
         $allPage.css({"overflow-y":"auto","max-height":"auto"});
         $(this).remove();
@@ -391,130 +391,79 @@ $.simpleDialog = function (args) {
  * 使用方式: img变迁使用data-url  添加class='lazy-loading'
  * 调用方式: $('lazy-loading').lazyLoading();
  ======================================================*/
-$.fn.lazyLoading = function (options) {
-    var defaults = {attr: "data-url", container: $(window), callback: $.noop};
-    var params = $.extend({}, defaults, options || {});
-    params.cache = [];
-    $(this).each(function () {
-        var node = this.nodeName.toLowerCase(), url = $(this).attr(params["attr"]);
-        var data = {obj: $(this), tag: node, url: url};
-        params.cache.push(data)
-    });
-    var callback = function (call) {
-        if ($.isFunction(params.callback)) {
-            params.callback.call(call.get(0))
-        }
-    };
-    var loading = function () {
-        var contHeight = params.container.height();
-        if (params.container.get(0) === window) {
-            contop = $(window).scrollTop()
-        } else {
-            contop = params.container.offset().top
-        }
-        $.each(params.cache, function (i, data) {
-            var o = data.obj, tag = data.tag, url = data.url, post, posb;
-            if (o) {
-                post = o.parent().offset().top - contop, posb = post + o.height();
-                if ((post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight)) {
-                    if (url) {
-                        if (tag === "img") {
-                            callback(o.attr("src", url))
-                        } else {
-                            o.load(url, {}, function () {
-                                callback(o)
-                            })
-                        }
-                    } else {
-                        callback(o)
-                    }
-                    data.obj = null
-                }
-            }
-        })
-    };
-    loading();
-    params.container.bind("scroll", loading)
-};
-/**====================================================
- *************** 其它扩展方法  ***********************
- ======================================================*/
-//点击事件  去重
-$.fn.clickEvent = function(callback){
-    if("ontouchstart" in window){
-        var data_fb = this.data("fb");
-        data_fb && typeof data_fb.destroy === "function" && data_fb.destroy();
-        var fb = new FastButton(this[0],callback);
-        this.data("fb",fb);
-    }else{
-        this.off("click");
-        this.on("click",callback);
-    }
-};
-$.fn.removeClickEvent = function(){
-    if("ontouchstart" in window){
-        var data_fb = this.data("fb");
-        data_fb && typeof data_fb.destroy === "function" && data_fb.destroy();
-    }else{
-        this.off("click");
-    }
-};
-/**============================================================
- ********************  图片裁剪  ********************
- ==============================================================*/
-$.fn.imgClip = function(){
-    var _ = this[0];
-    //判断浏览器是否支持FileReader接口
-    if(typeof FileReader == 'undefined'){
-        alert("你的浏览器不支持FileReader接口！");
-        //使选择控件不可操作
-        _.setAttribute("disabled","disabled");
-        return false;
-    }
-    this.on("change",function(){
-        var file = _.files[0];
-        if(!/image\/\w+/.test(file.type)){
-            Common.simpleMessage({msg:"请选择图片类型文件！",top:"45%"});
-            return false;
-        }
-        var reader = new FileReader();
-        //将文件以Data URL形式读入页面
-        reader.readAsDataURL(file);
-        reader.onload=function(e){
-            var arr = [];
-            arr.push('<div class="popup popup-about" id="popup_imgClip">');
-            arr.push('      <header class="bar bar-nav">');
-            arr.push('          <a class="button button-link button-nav pull-left close-popup" href="#">');
-            arr.push('              <span class="icon icon-left"></span>返回');
-            arr.push('          </a>');
-            arr.push('          <h1 class="title">裁剪</h1>');
-            arr.push('          <button class="button pull-right btn-use">使用</button>');
-            arr.push('      </header>');
-            arr.push('      <div class="content">');
-            arr.push('          <div class="list-block">');
-            arr.push('              <iframe frameborder="0" width="100%" id="imgClipIframe" src="imgClip.html"></iframe>');
-            arr.push('          </div>');
-            arr.push('      </div>');
-            arr.push('</div>');
-            $.popup(arr.join(''));
-            //css
-            var $pop = $("#popup_imgClip");
-            var contentH = $pop.find(".content").height();
-            var $iframe = $("#imgClipIframe");
-            $iframe.height(contentH);
-            //显示图片
-            var img = this.result;
-            $iframe[0].onload=function(){
-                this.contentWindow.init(img,contentH);
-            };
-            //event
-            $pop.find(".btn-use").clickEvent(function(){
-                $iframe[0].contentWindow.imgUse();
-            })
-        }
-    });
-};
++function () {
+    'use strict';
 
+    $.fn.lazyLoading = function (options) {
+        var defaults = {attr: "data-url", container: $(window), callback: $.noop};
+        var params = $.extend({}, defaults, options || {});
+        params.cache = [];
+        $(this).each(function () {
+            var node = this.nodeName.toLowerCase(), url = $(this).attr(params["attr"]);
+            var data = {obj: $(this), tag: node, url: url};
+            params.cache.push(data)
+        });
+        var callback = function (call) {
+            if ($.isFunction(params.callback)) {
+                params.callback.call(call.get(0))
+            }
+        };
+        var loading = function () {
+            var contop;
+            var contHeight = params.container.height();
+            if (params.container.get(0) === window) {
+                contop = $(window).scrollTop()
+            } else {
+                contop = params.container.offset().top
+            }
+            $.each(params.cache, function (i, data) {
+                var o = data.obj, tag = data.tag, url = data.url, post, posb;
+                if (o) {
+                    post = o.parent().offset().top - contop;
+                    posb = post + o.height();
+                    if ((post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight)) {
+                        if (url) {
+                            if (tag === "img") {
+                                callback(o.attr("src", url))
+                            } else {
+                                o.load(url, {}, function () {
+                                    callback(o)
+                                })
+                            }
+                        } else {
+                            callback(o)
+                        }
+                        data.obj = null
+                    }
+                }
+            })
+        };
+        loading();
+        params.container.bind("scroll", loading)
+    };
+
+    //全局调用
+    $(function () {$('.lazy-loading').lazyLoading()});
+}();
+/****************************************************************
+ * 图片延迟加载 使用方法:元素上加 data-src=""  或  data-background=""
+ ************************************************************/
++function () {
+    "use strict";
+    window.addEventListener('load',function () {
+        var $delayImg = $('.delay-loading');
+        $delayImg.each(function () {
+            var src = this.dataset.src;
+            if(typeof src != "undefined"){
+                $(this).attr('src',src);
+            }
+            var bg = this.dataset.background;
+            if(typeof bg != "undefined"){
+                $(this).css("background-image",bg);
+            }
+        });
+    },false);
+}();
 /**=========================================
  * @param btnID  上传控件ID不用带#   必填
  * @param type  类型  必填
@@ -933,3 +882,175 @@ $.imgUpload = function(btnID,type,maxSize,showImg,fn){
     $(document).on('keydown','input[data-width="auto"]',keydownHandle);
     $(document).on('keyup','input[data-width="auto"]',keyupHandle);
 }($);
+/*********************************************
+ * ---  标签插件  ---
+ **********************************************/
++function () {
+    'use strict';
+
+    //参数
+    var options = {};
+
+    //获取个性标签数据
+    function getValues(url) {
+        return ["标签"+Math.round(Math.random()*100), "标签"+Math.round(Math.random()*100), "标签"+Math.round(Math.random()*100),"标签"+Math.round(Math.random()*1000), "标签"+Math.round(Math.random()*1000), "标签"+Math.round(Math.random()*1000)];
+    }
+    //计算当前已选数目
+    function getChooseNumber() {
+        var $lis = $('.hp-label-box').find('.checked-label-list li');
+        if($lis.length > 0){
+            return $lis.length;
+        }
+        return 0;
+    }
+    //关闭
+    function close() {
+        var $box = $('.hp-label-box');
+        $box.length > 0 && $box.removeClass("zoomIn").addClass("zoomOut");
+        $.unlockScreen();
+    }
+    //换一换
+    function changeLabel(url) {
+        var values = getValues(url) || [];
+        var arr = [];
+        var $list = $('.hp-label-box .label-list');
+        if(values.length > 0){
+            $.each(values, function () {
+                arr.push('<li>'+this+'</li>');
+            });
+            $list.html(arr.join(''));
+        }
+        $list.find('li').on('click', function () {
+            if(getChooseNumber() >= options.maxChoose){
+                alert('最多只能选择'+options.maxChoose+'个');
+            }else{
+                labelClickHandle.call(this);
+            }
+        })
+    }
+    //确定
+    function confirmHandle(required, cb) {
+        var $box = $('.hp-label-box');
+        var $checkedList = $box.find('.checked-label-list');
+        var list = [];
+        $checkedList.find('li').each(function () {
+            list.push($(this).attr('data-text'))
+        });
+        if(required && list.length === 0){
+            alert('至少选择一个标签');
+            return false;
+        }
+        //回调
+        cb(list);
+        //关闭
+        close();
+    }
+    //选择label
+    function labelClickHandle() {
+        var $box = $('.hp-label-box');
+        var $list = $box.find('.label-list');
+        var $checkedList = $box.find('.checked-label-list');
+        $(this).addClass('checked').attr('data-index',$(this).index()).attr('data-text', $(this).text());
+        $checkedList.append($(this).clone().append('<a class="remove">&times;</a>'));
+
+        var clickH = function () {
+            var $li = $(this).closest('li');
+            var index = $li.attr('data-index');
+            var $labelLi = $list.find('li').eq(index);
+            if($labelLi.text() == $li.attr('data-text')){
+                $labelLi.removeClass('checked');
+            }
+            //remove
+            $li.remove();
+        };
+        $checkedList.find('li .remove').off('click').on('click', clickH);
+    }
+    //显示
+    function showLabelBox(opt){
+        options = $.extend({
+            required:true, //是否必选
+            title: '个性标签',
+            maxChoose: 3,  //最多选择标签数
+            parentDom:'',
+            url:'',
+            callback:function () {}
+        },opt || {});
+
+        var defaultValues = getValues(options.url);
+
+        var arr = [];
+        arr.push('<div class="hp-label-box animated zoomIn">');
+        arr.push('      <div class="hp-label">');
+        arr.push('          <div class="hp-label-header">');
+        arr.push('              <h3>'+options.title+'</h3>');
+        arr.push('              <p>选择您的标签，最多选择'+options.maxChoose+'个</p>');
+        arr.push('          </div>');
+        arr.push('          <div class="hp-label-body">');
+        arr.push('              <div class="hlb-top">');
+        arr.push('                  <ul class="checked-label-list">');
+
+        arr.push('                  </ul>');
+        arr.push('              </div>');
+        arr.push('              <div class="hlb-bottom">');
+        arr.push('                  <ul class="label-list">');
+        $.each(defaultValues, function () {
+            arr.push('                  <li>'+this+'</li>');
+        });
+        arr.push('                  </ul>');
+        arr.push('              </div>');
+        arr.push('          </div>');
+        arr.push('          <div class="hp-label-footer">');
+        arr.push('              <a class="btn-change">换一换</a>');
+        arr.push('          </div>');
+        arr.push('      </div>');
+        arr.push('<a class="btn-confirm"></a>');
+        arr.push('</div>');
+
+        $(options.parentDom).html(arr.join(''));
+
+        var $box = $('.hp-label-box');
+        //change btn
+        $box.find('.btn-change').on('click', function () {
+            changeLabel.call(null,options.url)
+        });
+        //confirm btn
+        $box.find('.btn-confirm').on('click', function () {
+            confirmHandle.call(null, options.required, options.callback);
+        });
+        //label click
+        $box.find('.label-list li').on('click', function () {
+            if($(this).hasClass('checked')){
+                var $box = $('.hp-label-box');
+                var $checkedList = $box.find('.checked-label-list');
+                var text = $(this).text();
+                $checkedList.find('li').each(function () {
+                    if($(this).attr('data-text') == text){
+                        $(this).remove();
+                    }
+                });
+                $(this).removeClass('checked');
+            }else{
+                if(getChooseNumber() >= options.maxChoose){
+                    alert('最多只能选择'+options.maxChoose+'个');
+                }else{
+                    labelClickHandle.call(this);
+                }
+            }
+        })
+    }
+    function label(options) {
+        var self = this;
+
+        $(this).off('click').on('click',function () {
+            var $box = $('.hp-label-box');
+            $.lockScreen();
+            if($box.length > 0){
+                $box.removeClass('zoomOut').addClass('zoomIn');
+            }else {
+                showLabelBox.call(self, options)
+            }
+        });
+    }
+
+    $.fn.label = label;
+}();
